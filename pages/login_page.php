cd7b8b44b36decaa598b24407704fcb617117f00
Changxing <jimmy75792@gmail.com>
2020-09-12 01:29:28 +1200

    Login page scratch

---
+++ pages/login_page.php
@@ -0,0 +1,26 @@
+<DOCTYPE HTML>
+    <html>
+    <head>
+        <title>
+            Tserver
+        </title>
+    </head>
+    <body>
+    <form method="POST" action="login_page.php">
+            <pre>
+                Username
+                <input type="text" name="username" placeholder="Please enter your username" autocomplete="on"
+                       required="required">
+                Password
+                <input type="password" name="password" placeholder="Please enter your password" required="required"
+                       minlength="6">
+                <input type="submit" value="login">
+            </pre>
+    </form>
+    </body>
+    </html>
+</DOCTYPE>
+
+<?php
+?>
+
