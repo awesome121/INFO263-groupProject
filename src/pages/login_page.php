aa02c9c99586acc4e5c69e4cb30815fd89e99d47
Changxing <jimmy75792@gmail.com>
2020-09-19 16:48:51 +1200

    1. Add verifyAccount function in login_page.php 2. Redirect to home_page.php if the account is valid

--- src/pages/login_page.php
+++ src/pages/login_page.php
@@ -21,6 +21,36 @@
     </body>
 </html>
 
-<?php
-?>
 
+<?php
+require_once('../db_config.php');
+$conn = new mysqli($hostname, $username, $password, $database);
+if($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
+$user_info = verifyAccount($conn);
+if ($_POST["username"] != "" && $_POST["password"] != "") {
+    if ($user_info != "") {
+        header("Location: home_page.php");
+    } else {
+        print_r("Invalid username or password");
+    }
+}
+
+
+function verifyAccount($conn) {
+    $query = "call get_user();";
+    $result = mysqli_query($conn, $query);
+    $input_username = $_POST["username"];
+    $input_password = $_POST["password"];
+    $row = mysqli_fetch_row($result);
+    while($row != NULL){
+        if ($input_username==$row[0] && $input_password==$row[1]) {
+            $email = $row[2];
+            $fullName = $row[3];
+            return array($email, $fullName);
+        }
+        $row = mysqli_fetch_row($result);
+    }
+    return "";
+}
+
+?>
