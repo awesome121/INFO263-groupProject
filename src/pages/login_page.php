28819e554a2fc054a4a4ebcee457c963a0acde58
emilyjs28 <70866427+emilyjs28@users.noreply.github.com>
2020-09-21 14:32:21 +1200

    Not done yet but this is where its at for now

--- src/pages/login_page.php
+++ src/pages/login_page.php
@@ -1,23 +1,51 @@
 <!DOCTYPE html>
 <html>
+
     <head>
-        <title>
-            Tserver
-        </title>
+        <title>Login - TServer</title>
+
+        <!-- Bootstrap CSS -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous" />
     </head>
 
     <body>
-        <form method="POST" action="login_page.php">
-                <pre>
-                    Username
-                    <input type="text" name="username" placeholder="Please enter your username" autocomplete="on"
-                           required="required">
-                    Password
-                    <input type="password" name="password" placeholder="Please enter your password" required="required"
-                           minlength="6">
-                    <input type="submit" value="login">
-                </pre>
-        </form>
+        <!-- Logo -->
+        <div>
+            <a class="container-fluid row text-center mt-4 mb-3"  href="#">
+                <img src="../images/UC_logo.png" class="rounded mx-auto d-block" height="150">
+            </a>
+        </div>
+
+ <!--   Original Form Code
+        <div>
+            <form method="POST" action="login_page.php">
+                    <pre>
+                        Username
+                        <input type="text" name="username" placeholder="Please enter your username" autocomplete="on"
+                               required="required">
+                        Password
+                        <input type="password" name="password" placeholder="Please enter your password" required="required"
+                               minlength="6">
+                        <input type="submit" value="login">
+                    </pre>
+            </form>
+        </div> -->
+
+        <!--Reworked Form Code-->
+        <div class="container-fluid row text-center">
+                <div class="col text-center">
+                    <div class="info-form">
+                        <form method="POST" action="login_page.php">
+
+                                <input type="text" name="username" placeholder="Username" autocomplete="on"
+                                       required="required"><br>
+                                <input type="password" class="password mt-2" name="password" placeholder="Password" required="required"
+                                       minlength="6"><br>
+                                <button type="button" class="btn btn-secondary mt-3" value="login" >Login</button>
+                        </form>
+                    </div>
+                </div>
+        </div>
     </body>
 </html>
 
@@ -29,7 +57,7 @@ if($conn->connect_error) {die("Connection failed: " . $conn->connect_error);}
 $user_info = verifyAccount($conn);
 if ($_POST["username"] != "" && $_POST["password"] != "") {
     if ($user_info != "") {
-        header("Location: home_page.php");
+        header("Location: home.php");
     } else {
         print_r("Invalid username or password");
     }
