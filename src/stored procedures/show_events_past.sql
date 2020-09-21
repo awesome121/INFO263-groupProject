c34406cb6d752d38ae4f2bbda07e931c088dda55
awesome121 <51814631+awesome121@users.noreply.github.com>
2020-09-21 13:20:53 +1200

    Update show_events_past.sql

--- src/stored procedures/show_events_past.sql
+++ src/stored procedures/show_events_past.sql
@@ -1,4 +1,4 @@
-CREATE DEFINER=`lcs57`@`%` PROCEDURE `show_events_past`()
+CREATE DEFINER=`xxx`@`%` PROCEDURE `show_events_past`().  //Please put your username in xxx
 BEGIN
 	SELECT * FROM vw_front_event WHERE date <= curdate() AND date >= "2020/01/01"
     ORDER BY date DESC;
