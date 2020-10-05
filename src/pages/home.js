271bcd5d79042ad2f6563ab7e1e73c86a389e6ef
awesome121 <51814631+awesome121@users.noreply.github.com>
2020-10-06 12:23:05 +1300

    Calendar feature done!

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,26 +1,5 @@
-// $( "#myInput" ).keyup(function() {
-//     while (document.getElementById("myDropdown").childNodes.length != num) {
-//         document.getElementById("myDropdown").childNodes.item(num).remove();
-//     }
-//     var input_text = document.getElementById("myInput").value;
-//     if (input_text != "") {
-//         var mysql = require('mysql');
-//
-//         var con = mysql.createConnection({
-//             host: "localhost",
-//             user: "root",
-//             password: "mysql",
-//         });
-//
-//         con.connect(function(err) {
-//             if (err) throw err;
-//             console.log("Connected!");
-//         });
-//         document.getElementById("myDropdown").insertAdjacentHTML("beforeend", "<a>No Events Find</a>");
-//     }
-// });
 
-function showResult(keyword) {
+function showSearchResult(keyword) {
 
     if (keyword.toString().length < 3) {
         while (document.getElementById("hint").children.length != 0)
@@ -42,27 +21,62 @@ function showResult(keyword) {
 }
 
 
+function calendarSwitch(left){
+    var date = document.getElementById("currentDate").innerText.split(" - ");
+    var startDate = new Date(date[0]);
+    var endDate =  new Date(date[1]);
+    if (left) { //minus a week
+        startDate.setDate(startDate.getDate()-7);
+        endDate.setDate(endDate.getDate()-7);
+
+    } else { // plus a week
+        startDate.setDate(startDate.getDate()+7);
+        endDate.setDate(endDate.getDate()+7);
+    }
+    const dateTimeFormat = new Intl.DateTimeFormat();
+    startDate = dateTimeFormat.format(startDate).split('/').reverse();
+    endDate = dateTimeFormat.format(endDate).split('/').reverse();
+    if (startDate[2].length == 1){
+        startDate[2] = '0' + startDate[2];
+    }
+    if (endDate[2].length == 1){
+        endDate[2] = '0' + endDate[2];
+    }
+    startDate = startDate.join('-');
+    endDate = endDate.join('-');
+
+    var xmlhttp = new XMLHttpRequest();
+    xmlhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            var parser = new DOMParser();
+            var responseDoc = parser.parseFromString(this.responseText, "text/html")
+            document.getElementById("mainFrame").innerHTML = responseDoc.getElementById("mainFrame").innerHTML;
+            document.getElementById("currentDate").innerHTML = responseDoc.getElementById("currentDate").innerHTML;
+        }
+    }
+    xmlhttp.open("GET", "home.php?startDate=" + startDate + "&endDate=" + endDate, true);
+    xmlhttp.send();
+}
 
 
 
 
 
-// document.addEventListener("click", function (event) {
-//     console.log(event);
-// });
-console.log(document.getElementById("card1"));
+var showButtons = document.getElementsByClassName("btn-primary");
+for (var i=0; i < showButtons.length; i++){
+    var button = showButtons.item(i);
+    button.addEventListener("click", function(){
+        if (this.innerHTML == "Show less"){
+            this.innerHTML = "Show more";
+        } else {
+            this.innerHTML = "Show less";
+        }
 
-// (".btn-primary").addEventListener("click", function(){
-//     // if ()
-//     var text = document.getElementById("card1").innerText;
-//     if (text == "Show  less"){
-//         document.getElementById("card1").innerText = "Show more";
-//     } else {
-//         document.getElementById("card1").innerText = "Show less";
-//     }
-//
-// });
+    });
+}
+
 
 document.getElementById("myDropdown").classList.toggle("show");
-console.log($('.card-header').width);
 console.log("Ready");
+
+
