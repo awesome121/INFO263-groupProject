719225c70cbcce54a6bfb188d3f3bac4e673b3fe
awesome121 <51814631+awesome121@users.noreply.github.com>
2020-10-16 11:21:11 +1300

    Update db_config.php

--- src/db_config.php
+++ src/db_config.php
@@ -1,21 +1,19 @@
 <?php
-
-//$hostname = "localhost";
-//$username = "root";  //Changxing's account
-//$password = "mysql";
-//$database = "t_server"
-
+/*
+ * database configuration
+ * Please execute stored_procedure.sql before trying to test our database
+ *
+ */
 
 
 //Leilani's database info
-$hostname = "ucmysqlacis01p.linux.canterbury.ac.nz";
-$database = "INFO263_lcs57_tserver";
-$username = "lcs57";
-$password = "Vezr050_";
-$acct = "INFO263_lcs57_tserver"
+//$hostname = "ucmysqlacis01p.linux.canterbury.ac.nz";
+//$database = "INFO263_lcs57_tserver";
+//$username = "lcs57";
+//$password = "Vezr050_";
 
 
-
+// Changxing's account
 //$hostname = "ucmysqlacis01p.linux.canterbury.ac.nz";
 //$database = "INFO263_cgo54_tserver";
 //$username = "cgo54";
