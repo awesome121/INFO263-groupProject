ce9b2a5c9fa2ab66934b715b44153270a8af1fc0
Changxing Gong <51814631+awesome121@users.noreply.github.com>
2021-02-04 22:23:06 +1300

    Update db_config.php

--- src/db_config.php
+++ src/db_config.php
@@ -4,6 +4,9 @@
  * Please execute stored_procedure.sql before trying to test our database
  *
  */
-
+$hostname = "localhost";
+$username = "root";  //Changxing's account
+$password = "mysql";
+$database = "t_server"
 ?>
 
