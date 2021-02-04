c0a68c9339104ba6629f07e18238e571f9296b76
Changxing Gong <51814631+awesome121@users.noreply.github.com>
2021-02-04 22:22:42 +1300

    Update db_config.php

--- src/db_config.php
+++ src/db_config.php
@@ -5,18 +5,5 @@
  *
  */
 
-
-//Leilani's database info
-//$hostname = "ucmysqlacis01p.linux.canterbury.ac.nz";
-//$database = "INFO263_lcs57_tserver";
-//$username = "lcs57";
-//$password = "Vezr050_";
-
-
-// Changxing's account
-//$hostname = "ucmysqlacis01p.linux.canterbury.ac.nz";
-//$database = "INFO263_cgo54_tserver";
-//$username = "cgo54";
-//$password = "Exjg955_";
 ?>
 
